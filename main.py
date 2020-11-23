80387e87a55cc565174c4822124ca8e884f7ddd5
Cescobar9 <56111778+Cescobar9@users.noreply.github.com>
2020-11-22 19:46:05 -0800

    Add files via upload

--- main.py
+++ main.py
@@ -12,7 +12,7 @@ def index():
 
 @app.route('/index')
 def home():
-   return render_template('index.html')
+    return render_template('index.html')
 
 
 @app.route('/generic')
@@ -33,18 +33,20 @@ def test():
     distance = request.args.get('distance')
     print(location, distance, search)
 
-    if not all((location, distance, search)):
+    if not location and not distance and not search:
         return  '<h1> We require a zip code, a radius in miles, and a search query in order to find products in stock. </h1>'
+    elif not location and not distance:
+        return '<h1> Error: The Zip Code and distance field are missing. </h1>'
+    elif not distance and not search:
+        return '<h1> Error: The distance and query search field are missing. </h1>'
+    elif not location and not search:
+        return '<h1> Error: The location and query search field are missing. </h1>'
     elif not location:
         return '<h1> Error: Zip Code search field is missing. </h1>'
     elif not distance:
         return '<h1> Error: Distance search field is missing. </h1>'
     elif not search:
         return '<h1> Error: The query search field is missing. </h1>'
-    elif not location and not distance:
-        return '<h1> Error: The Zip Code and distance field is missing. </h1>'
-    elif not distance and not search:
-        return '<h1> Error: The distance and query search field is missing. </h1>'
 
     stores = walmart.Stores(location, distance)
     tStore = target.tStores(location, distance)
@@ -59,7 +61,9 @@ def test():
     # use the lowest key for testing purposes
 
     if not data and not targetData:
-     return '<h1> There is no store within the specified distance. <h1>'
+        return '<h1> There is no store within the specified distance. </h1>'
+
+    search_results = [] # search results for both Walmart and Target to render generic.html
 
     if data:
         # initialize the query with a store id and query
@@ -70,27 +74,21 @@ def test():
 
         # results is a list of the Results dataclass
         results = query.fetch_results(5)
-
-        result_string = "<h1>Walmart</h1><ul>"
+        walmart_cart = []
 
         for result in results:
-            # result_string += '<li>' + (str(result)) + '</li>'
-            result_string += '<li>'
-            result_string += "<ul>"
-
-            result_string += '<li>' + result.brand + '</li>'
-            result_string += '<li>' + result.name + '</li>'
-            result_string += '<li>' + result.availability + '</li>'
-            result_string += '<li>' + '${:0.2f}'.format(float(result.price)) + '</li>'
-            result_string += '<li>' + result.address + '</li>'
-            result_string += '<li>' + f'Distance from your location: {result.distance} miles' + '</li>'
-            result_string += f'<img src="{result.image}" alt="img" />'
-
-            result_string += '</ul>'
-            result_string += '</li>'
-
-        result_string += '</ul>'
-        del query
+            if result.availability.lower() == 'in stock' or 'limited' in result.availability.lower():
+                w_item_dict = {}
+                # each results carries 7 pieces of info
+                w_item_dict['brand'] = result.brand
+                w_item_dict['name'] = result.name
+                w_item_dict['availability'] = result.availability
+                w_item_dict['price'] = '${:0.2f}'.format(float(result.price))
+                w_item_dict['address'] = result.address
+                w_item_dict['distance'] = f'Distance: {result.distance}'
+                w_item_dict['image'] = result.image
+                walmart_cart.append(w_item_dict)
+        search_results.append(walmart_cart)  # add items from walmart search results to final results list
 
     if targetData:
         # initialize the query with a store id and query
@@ -99,29 +97,22 @@ def test():
         
         # request api data and results a list
         tquery.tSearch()
-
-        result_string += "<h1>Target</h1><ul>"
+        target_cart = []
 
         for result in tquery.results[:5]:
-        # result_string += '<li>' + (str(result)) + '</li>'
-            result_string += '<li>'
-            result_string += "<ul>"
+                t_item_dict = {}
+                t_item_dict['brand'] = result.brand
+                t_item_dict['name'] = result.name
+                t_item_dict['availability'] = result.availability
+                t_item_dict['price'] = '${:0.2f}'.format(float(result.price))
+                t_item_dict['address'] = result.address
+                t_item_dict['distance'] = f'Distance: {result.distance}'
+                t_item_dict['image'] = result.image
+                target_cart.append(t_item_dict)
+        search_results.append(target_cart)
 
-            result_string += '<li>' + result.brand + '</li>'
-            result_string += '<li>' + result.name + '</li>'
-            result_string += '<li>' + result.availability + '</li>'
-            result_string += '<li>' + '${:0.2f}'.format(float(result.price)) + '</li>'
-            result_string += '<li>' + result.address + '</li>'
-            result_string += '<li>' + f'Distance from your location: {result.distance} miles' + '</li>'
-            result_string += f'<img src="{result.image}" alt="img" />'
-
-            result_string += '</ul>'
-            result_string += '</li>'
-        result_string += '</u>'
-
-    return result_string
+    return render_template('generic.html', results=search_results)
 
 
 if __name__ == '__main__':
-    app.debug = True
-    app.run()
+    app.run(debug=True)
---
+++ selenium-tests/item-tests/item-1b.py
@@ -0,0 +1,44 @@
+from selenium import webdriver
+from selenium.webdriver.support.ui import WebDriverWait
+from time import sleep
+
+'''this test checks valid input for zip code field'''
+
+
+def get_driver():
+    # location of chrome driver for windows
+    chromedriver = "D:/Selenium-Drivers/Chrome-Driver/chromedriver_win32/chromedriver"
+    return webdriver.Chrome(chromedriver)
+
+
+def clear(field_list):
+    for field in field_list:
+        field.clear()
+
+
+def item_valid_test():
+    driver = get_driver()  # web driver component
+    sleep(5)  # wait 5 seconds for elements to load on page before searching for them
+    driver.maximize_window()
+    driver.get("https://skudar.pythonanywhere.com/")  # launch web app
+    sleep(5)
+
+    # grab field components
+    field_list = driver.find_elements_by_tag_name('input')
+    clear(field_list)  # clear field inputs
+    field_list[2].send_keys('headphones')  # item input
+    sleep(3)
+    field_list[0].send_keys('90242')  # zipcode input
+    field_list[1].send_keys('10')  # distance input
+    sleep(3)  # pause for 3 seconds to show inputs in fields
+
+    # click search button
+    search_btn = driver.find_element_by_xpath("//button")
+    search_btn.click()
+
+    sleep(6)
+    driver.close()  # close window
+
+
+if __name__ == "__main__":
+    item_valid_test()
